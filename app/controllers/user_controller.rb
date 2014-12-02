d0a2c8d7b4e81ea8b5d928faaf9b6ba40346e3f5
shaichun <yichun03@icloud.com>
2014-12-03 02:40:47 +0800

    finish issue #8

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,24 +1,47 @@
 class UserController < ApplicationController
 
 
-  def new
-    @user = User.new
+
+  def getToken
+    render :text => form_authenticity_token
   end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to root, :notice => "Sign up"
+
+  #1. 輸入是空的會彈出錯誤
+  #2.
+  #
+  def register
+    input = register_params
+
+    name = input[:name]
+    email = input[:email]
+    password = input[:password]
+    password_confirmation = input[:password_confirmation]
+
+    unless password == password_confirmation
+      response = 'UMSE04'
     else
-      render new
+      if User::find_by_email email
+        response = 'UMSE03'
+      else
+        user = User.new(:email => email,:name =>name,:password => password,:sign_in_count => 0)
+        user.save
+        response = 'success'
+      end
     end
+
+    render :json => response.to_json
+
   end
 
-  def update
+  private
 
-  end
-
-  def destroy
-
+  def register_params
+    {
+        :name => params.require(:name),
+        :email => params.require(:email),
+        :password => params.require(:password),
+        :password_confirmation => params.require(:password_confirmation)
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :user
+  #resources :user
+  post 'user/register' => 'user#register'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
