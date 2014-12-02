4c1b0bac3b9b5fbabf4eb6def64a3a16f91e6bf0
Raccoon <a60814billy@gmail.com>
2014-12-03 00:34:15 +0800

    finish Issue #7

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,16 +2,25 @@ class UserController < ApplicationController
 
 
   def new
-    @user = User.new
   end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to root, :notice => "Sign up"
+  def login
+    email = params[:email]
+    password = params[:password]
+
+    user = User::find_by_email email
+
+    if user
+      if user.authenticate(password)
+        session[:login_user] = email
+        response = 'success'
+      else
+        response = 'UMSE01'
+      end
     else
-      render new
+      response = 'UMSE02'
     end
+    render :json => response.to_json
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :user
+  post 'user/login' => 'user#login'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
