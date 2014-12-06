ff4ba7159554482a9b70c7fdadcbd664bdc7c03f
shaichun <yichun03@icloud.com>
2014-12-06 16:55:14 +0800

    current_user 實作

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,9 @@
 module UserHelper
+  def current_user
+    if session.include?(:login_user)
+      email = session[:login_user]
+      user = User.find_by_email email
+      return user
+    end
+  end
 end
