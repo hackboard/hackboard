3955a0fcbd3ce2633528301909f916e2bf7ab4bc
shaichun <yichun03@icloud.com>
2014-12-20 11:34:52 +0800

    test test

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
-
 class UserControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  def setup
+    @controller = Api::UserController.new
+  end
+
+  def teardown
+    session.delete :login_user
+  end
+
+  def test_login_success
+
+    # post(:login,{:email=>'alice@meigic.tw',:password=>'alice0000'})
+    # assert_equal(nil,@response.body)
+    # assert_response(200)
+  end
+
+  def test_login_failed
+    post(:login,{:email=>'XXXXX@meigic.tw',:password=>'XXXXX0000'})
+    assert_equal("UMSE02",@response.body)
+    assert_response(403)
+  end
+  def test_logout_failed
+    post(:logout)
+    assert_equal("UMSE05",@response.body)
+  end
+
+
+  def test_logout_success
+    session[:login_user] = 'alice@megic.tw'
+    post(:logout)
+    assert_equal("success",@response.body)
+  end
+
+  def test_logout_200
+    post(:logout)
+    assert_response(200)
+  end
 end
