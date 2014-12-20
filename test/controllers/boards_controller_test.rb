ead568e74531fc61f4f0dc119e15eaab5015fe66
shaichun <yichun03@icloud.com>
2014-12-20 13:24:12 +0800

    add boars_controller testcase

---
+++ test/controllers/boards_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class BoardsControllerTest < ActionController::TestCase
+
+  def setup
+    @controller = Api::BoardsController.new
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  #-- index --
+  test "test_index_success" do
+
+  end
+
+  test "test_index_DAPIE01" do
+
+  end
+
+  #-- create --
+  test "test_create_success" do
+
+  end
+
+  test "test_create_DAPIE02_forbidden" do
+
+  end
+
+  #-- destory --
+  test "test_destory_DAPIE03" do
+
+  end
+
+  test "test_destory_delete success" do
+
+  end
+
+  test "test_destory_Not login" do
+
+  end
+  #-- flows --
+  test "test_flows_DAPIE04" do
+
+  end
+
+  test "test_flows_Not login" do
+
+  end
+end
