1a89668fda68fa6b5351ca1ea18d13ffef03fa73
shaichun <yichun03@icloud.com>
2014-12-23 00:34:48 +0800

    add test data

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -11,16 +11,21 @@ class BoardsControllerTest < ActionController::TestCase
 
   #-- index --
   test "test_index_success" do
+    session[:login_user] = "bob@meigic.tw"
+    get(:index)
+    assert_equal("{\"pin\":[],\"other\":[{\"id\":1,\"name\":\"work1\",\"wip\":10,\"description\":\"Board1\",\"board_id\":null,\"user_id\":1,\"created_at\":\"2014-12-22T16:16:46.000Z\",\"updated_at\":\"2014-12-22T16:16:46.000Z\"}]}",@response.body)
 
   end
 
   test "test_index_DAPIE01" do
-
+    get(:index)
+    assert_equal("\"DAPIE01\"",@response.body)
+    assert_response(403)
   end
 
   #-- create --
   test "test_create_success" do
-
+    
   end
 
   test "test_create_DAPIE02_forbidden" do
